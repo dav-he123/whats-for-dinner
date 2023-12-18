bf4bf131324ec1033bffa62c8d99864994a18ed2
dav-123 <david.he07@yahoo.ca>
2023-12-18 11:53:31 -0500

    feat: installed ejs and moved files into folders for better organization, and included favourites page

--- server/index.js
+++ server/index.js
@@ -7,11 +7,16 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 
+app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-app.get("/", (req, res) => {
-  res.send("Hello!");
+app.get("/favourites", (req, res) => {
+  res.render("favourites", { title: "Express" });
+});
+
+app.get("/main", (req, res) => {
+  res.render("main", { title: "Express" });
 });
 
 app.listen(PORT, () => {
